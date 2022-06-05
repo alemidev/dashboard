4465c672abb442b6c9d1e411dedddefbe03633f4
alemi <me@alemi.dev>
2022-06-05 20:05:39 +0200

    feat: added wrapper for native background worker

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 mod app;
+mod util;
 
 use app::App;
+
+use crate::util::worker::{BackgroundWorker, NativeBackgroundWorker};
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
 	let native_options = eframe::NativeOptions::default();
 
-	eframe::run_native(
+	let worker = NativeBackgroundWorker::start();
+
+	eframe::run_native( // TODO replace this with a loop that ends so we can cleanly exit the background worker
 		"2b2t queue stats",
 		native_options,
 		Box::new(|cc| Box::new(App::new(cc))),
 	);
+
+	// worker.stop();
 }
