78c7982911897ea628e85b3eb1bd89a6098c6502
alemidev <me@alemi.dev>
2022-06-06 04:42:06 +0200

    fix: spawn tokio runtime in another thread

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 mod app;
 mod util;
 
-use app::App;
-
+use std::sync::{Arc, Mutex};
+use tokio::time::{sleep, Duration};
 use crate::util::worker::{BackgroundWorker, NativeBackgroundWorker};
+use crate::app::{App, data::store::{SQLiteDataStore, DataStorage}};
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
-fn main() {
+fn main() -> ! {
 	let native_options = eframe::NativeOptions::default();
+	
+	let mut store_path = dirs::data_dir().unwrap_or(std::path::PathBuf::from(".")); // TODO get cwd more consistently?
+	store_path.push("dashboard.db");
 
-	let worker = NativeBackgroundWorker::start();
+	println!("{}", store_path.as_path().to_str().unwrap());
+
+	let store = Arc::new(
+			SQLiteDataStore::new(store_path)
+				.unwrap()
+	);
 
 	eframe::run_native( // TODO replace this with a loop that ends so we can cleanly exit the background worker
-		"2b2t queue stats",
+		"dashboard",
 		native_options,
-		Box::new(|cc| Box::new(App::new(cc))),
+		Box::new(move |cc| {
+			let worker = NativeBackgroundWorker::start();
+			let ctx = cc.egui_ctx.clone();
+			worker.task(async move {
+				loop {
+					sleep(Duration::from_secs(1)).await;
+					ctx.request_repaint();
+					// tokio::spawn(async move {store2.fetch_all().await});
+				}
+			});
+
+			Box::new(App::new(cc, store))
+		}),
 	);
 
 	// worker.stop();
